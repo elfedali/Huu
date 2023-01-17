2a06eb2aa2105111abc27ce8248282a94c251fb0
Abdessamad EL FEDALI <a.elfedali@gmail.com>
2023-01-17 09:36:00 +0100

    Add login template

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
-})
-export class LoginComponent {
 
+})
+export class LoginComponent implements OnInit {
+
+  form!: FormGroup;
+  loading = false;
+  submitted = false;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      username: new FormControl('hello@mail.com', Validators.required),
+      password: new FormControl('', [Validators.required])
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
 }
+

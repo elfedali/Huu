39fd541d84dbeb64596e0abc4a8bd00696230590
Abdessamad EL FEDALI <a.elfedali@gmail.com>
2023-01-17 10:23:01 +0100

    forgot password template + 404

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.scss']
-})
-export class ForgotPasswordComponent {
 
+})
+export class ForgotPasswordComponent implements OnInit {
+  form!: FormGroup;
+  loading = false;
+  submitted = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
 }
+
+

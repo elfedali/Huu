8dd273d625635381611a27062eae3a06c0e2437e
Abdessamad EL FEDALI <a.elfedali@gmail.com>
2023-01-17 10:47:28 +0100

    Access Denied page

--- src/app/access-denied/access-denied.component.ts
+++ src/app/access-denied/access-denied.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-access-denied',
   templateUrl: './access-denied.component.html',
-  styleUrls: ['./access-denied.component.scss']
 })
 export class AccessDeniedComponent {
 

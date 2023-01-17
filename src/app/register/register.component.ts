a4ed4b61cd252f82cba6e8a015e5a11ed29bba95
Abdessamad EL FEDALI <a.elfedali@gmail.com>
2023-01-17 10:02:34 +0100

    Register teemplate

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  form!: FormGroup;
+  loading = false;
+  submitted = false;
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      username: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
 }
+
+
+

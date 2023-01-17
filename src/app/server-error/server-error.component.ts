844716e5e3a689585bb83b33c301ba7c0931e330
Abdessamad EL FEDALI <a.elfedali@gmail.com>
2023-01-17 10:52:40 +0100

    Server error page template

--- src/app/server-error/server-error.component.ts
+++ src/app/server-error/server-error.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server-error',
   templateUrl: './server-error.component.html',
-  styleUrls: ['./server-error.component.scss']
 })
 export class ServerErrorComponent {
 
